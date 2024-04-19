9390b21774e03abda27b09e9f2580a7b3a007297
Chetan <chetans@indiratrade.com>
2024-04-19 16:25:56 +0530

    update metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "Next Prompt",
+  description: "AI Prompt Sharing Application",
 };
 
 const RootLayout = ({ children }) => (
